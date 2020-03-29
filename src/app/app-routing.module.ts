0a51b06d59f9037bf78fac3ab6689b308343b889
rahdi <adrian.heiden@gmail.com>
2020-03-29 12:39:58 +0200

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './chat/chat.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChatComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
