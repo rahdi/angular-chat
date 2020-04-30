62d968ac6a5761cdfa9a3dbf2ffe0d8ed901641d
rahdi <adrian.heiden@gmail.com>
2020-04-30 19:49:18 +0200

    filled in

--- src/app/chat-message-list/chat-message-list.component.ts
+++ src/app/chat-message-list/chat-message-list.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Message } from '../message';
 
 @Component({
   selector: 'app-chat-message-list',
   templateUrl: './chat-message-list.component.html',
-  styleUrls: ['./chat-message-list.component.sass']
+  styleUrls: ['./chat-message-list.component.scss']
 })
 export class ChatMessageListComponent implements OnInit {
 
+  @Input() messages:Message[] = [];
+  @Input() myId:number = 0;
+  
   constructor() { }
 
   ngOnInit() {
--- src/app/chat-new-message-area/chat-new-message-area.component.ts
+++ src/app/chat-new-message-area/chat-new-message-area.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-chat-new-message-area',
   templateUrl: './chat-new-message-area.component.html',
-  styleUrls: ['./chat-new-message-area.component.sass']
+  styleUrls: ['./chat-new-message-area.component.scss']
 })
 export class ChatNewMessageAreaComponent implements OnInit {
+
+  @Output() sendButtonClicked: EventEmitter<string> = new EventEmitter();
+  message: string = "";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  sendMessage(){
+    if (this.message) {
+      this.sendButtonClicked.emit(this.message);
+      this.message = "";
+    }
   }
 
 }
