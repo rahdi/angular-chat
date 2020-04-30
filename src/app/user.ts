e66c0c204e3dc4f591c6bc5ddbbdf3ef8131e064
rahdi <adrian.heiden@gmail.com>
2020-04-30 19:34:33 +0200

    modified

--- src/app/user.ts
+++ src/app/user.ts
@@ -4,10 +4,9 @@ export class User {
   user_password: string;
   online: boolean;
 
-  constructor(user_id: number, user_name: string, user_password: string, online: boolean) {
+  constructor(user_id: number, user_name: string, user_password: string, online: boolean = false) {
      this.user_id = user_id;
      this.user_name = user_name;
      this.user_password = user_password;
-     this.online = false;
   }
 }
