d0d34e7f93011d575d3e444e11fd4bad001658c4
rahdi <adrian.heiden@gmail.com>
2020-04-30 19:32:30 +0200

    added "online" property

--- src/app/user.ts
+++ src/app/user.ts
@@ -2,10 +2,12 @@ export class User {
   user_id: number;
   user_name:string;
   user_password: string;
+  online: boolean;
 
-  constructor(user_id: number, user_name: string, user_password: string) {
+  constructor(user_id: number, user_name: string, user_password: string, online: boolean) {
      this.user_id = user_id;
      this.user_name = user_name;
-     this.user_password = user_password
+     this.user_password = user_password;
+     this.online = false;
   }
 }
