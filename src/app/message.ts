42ae483c0373bedec483384f9c9bf4628ef4c13e
rahdi <adrian.heiden@gmail.com>
2020-05-01 19:44:55 +0200

    filled in

--- src/app/message.ts
+++ src/app/message.ts
@@ -1,2 +1,22 @@
 export class Message {
+  message_id: number;
+  message_From_user_id: number;
+  message_to_user_id: number;
+  message_text: string;
+  message_date: Date;
+
+  constructor(
+    message_id: number,
+    message_From_user_id: number,
+    message_to_user_id: number,
+    message_text: string,
+    message_date: Date
+  ) {
+    this.message_id = message_id;
+    this.message_From_user_id = message_From_user_id;
+    this.message_to_user_id = message_to_user_id;
+    this.message_text = message_text;
+    this.message_date = message_date;
+  }
+
 }
